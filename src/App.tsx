99ea6f9b6d04a606ecbd56e425b460f9fb9553dc
Aiboo968 <188341417@qq.com>
2022-08-12 21:39:46 +0800

    '适配手机'

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 import Hero from './Hero'
 import Footer from './Footer'
-import About from './pages/about';
+import About from './pages/about'
 import ConnectBtn from './ConnectBtn'
 import Speaker from './Speaker'
 import Venue from './Venue'
@@ -10,17 +10,91 @@ import wamo from './assets/wamo.svg'
 import Content from './Content'
 import { useTranslation, Trans } from 'react-i18next'
 import i18n from 'i18next'
+import { useEffect } from 'react'
 const changeLanguage = () => {
   i18n.changeLanguage(i18n.language == 'en' ? 'zh' : 'en')
 }
 function App() {
   const { t } = useTranslation()
+  const isMobile = () => {
+    var userAgentInfo = navigator.userAgent
+    var mobileAgents = [
+      'Android',
+      'iPhone',
+      'SymbianOS',
+      'Windows Phone',
+      'iPad',
+      'iPod'
+    ]
+    var mobile_flag = false
+    //根据userAgent判断是否是手机
+    for (var v = 0; v < mobileAgents.length; v++) {
+      if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+        mobile_flag = true
+        break
+      }
+    }
 
+    var screen_width = window.screen.width
+    var screen_height = window.screen.height
+
+    //根据屏幕分辨率判断是否是手机
+    if (screen_width < 780) {
+      mobile_flag = true
+    }
+    return mobile_flag
+  }
+  useEffect(() => {}, [])
   return (
     <div className="App">
       <div className="text-[#FFF] w-full">
-        <img src="/wamo_hero_bg.png" className="hero-bg" />
-        <div className="header">
+        {isMobile() ? (
+          <img src="/mobile_hero.png" className="hero-bg " />
+        ) : (
+          <img src="/wamo_hero_bg.png" className="hero-bg" />
+        )}
+
+        <div className="header ">
+          <div className="box-container">
+            <div className="h-16 flex-between">
+              <div className="brand flex gap-4">
+                <img className="h-[40px]" src={wamo} alt="" />
+                <span className="text-[1rem]">{t('hero.summer')}</span>
+              </div>
+              {isMobile() ? (
+                <>
+                  <ConnectBtn label={t('header.connect')} />
+                </>
+              ) : (
+                <div className="menu flex items-center gap-6 text-sm">
+                  <a href="#" onClick={changeLanguage}>
+                    中/EN
+                  </a>
+                  <a href="#content">{t('header.content')}</a>
+                  <a href="#venue">{t('header.venue')}</a>
+                  <a href="#speaker">{t('header.speakers')}</a>
+
+                  <a href="#about">{t('header.about')}</a>
+                  <ConnectBtn label={t('header.connect')} />
+                </div>
+              )}
+            </div>
+            {isMobile() && (
+              <div className="menu flex-center gap-6 ">
+                <a href="#" onClick={changeLanguage}>
+                  中/EN
+                </a>
+                <a href="#content">{t('header.content')}</a>
+                <a href="#venue">{t('header.venue')}</a>
+                <a href="#speaker">{t('header.speakers')}</a>
+                <a href="#about">{t('header.about')}</a>
+              </div>
+            )}
+            <Hero />
+          </div>
+        </div>
+
+        {/* <div className="header">
           <div className="box-container">
             <div className="h-16 flex-between">
               <div className="brand flex gap-4">
@@ -33,16 +107,16 @@ function App() {
                 </a>
                 <a href="#content">{t('header.content')}</a>
                 <a href="#venue">{t('header.venue')}</a>
-                <a href="#speaker">{t('header.speakers')}</a>
-                {/* <a href="#">{t('header.nft')}</a> */}
-                {/* <a href="#">{t('header.sbt')}</a> */}
-                <a href="#about">{t('header.about')}</a>
+                <a href="#speaker">{t('header.speakers')}</a> */}
+        {/* <a href="#">{t('header.nft')}</a> */}
+        {/* <a href="#">{t('header.sbt')}</a> */}
+        {/* <a href="#about">{t('header.about')}</a>
                 <ConnectBtn label={t('header.connect')} />
               </div>
             </div>
             <Hero />
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="bg-[#6ce4D1] py-32 relative">
         <div className="box-container blockquote relative">
--- src/Footer.tsx
+++ src/Footer.tsx
@@ -7,70 +7,169 @@ const changeLanguage = () => {
 }
 function Footer() {
   const { t } = useTranslation()
-  const openSocial = (url:string) => {
+  const openSocial = (url: string) => {
     window.open(url)
+  }
+  const isMobile = () => {
+    var userAgentInfo = navigator.userAgent
+    var mobileAgents = [
+      'Android',
+      'iPhone',
+      'SymbianOS',
+      'Windows Phone',
+      'iPad',
+      'iPod'
+    ]
+    var mobile_flag = false
+    //根据userAgent判断是否是手机
+    for (var v = 0; v < mobileAgents.length; v++) {
+      if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+        mobile_flag = true
+        break
+      }
+    }
+
+    var screen_width = window.screen.width
+    var screen_height = window.screen.height
+
+    //根据屏幕分辨率判断是否是手机
+    if (screen_width < 780) {
+      mobile_flag = true
+    }
+    return mobile_flag
   }
   return (
     <div className="bg-black">
-      <div className="box-container flex-between pt-12 pb-8 border-b border-white text-white mt-[2rem]">
-        <div className="flex gap-4">
+      {isMobile() ? (
+        <div className="box-container footer-mobile pt-12 pb-8 border-b border-white text-white mt-[2rem]">
           <img src={wamo} alt="summer of wamo" />
-          <div className="flex flex-col gap-2">
-            <div>Summer of WAMO</div>
-            <div className="text-sm">A Web3 Fest in Dali</div>
+          <div className="flex gap-4">
+            <div className="flex flex-col gap-2">
+              <div className="mt-4 text-lg center" style={{textAlign:'center'}}>Summer of WAMO</div>
+              <div className='text-lg'>A Web3 Fest in Dali</div>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="flex items-center gap-x-8">
-            <a href="#" className="text-white" onClick={changeLanguage}>
-              中 / EN
-            </a>
-            <a href="#content" className="text-white">
-              {t('header.content')}
-            </a>
-            <a href="#venue" className="text-white">
-              {t('header.venue')}
-            </a>
-            <a href="#speaker" className="text-white">
-              {t('header.speakers')}
-            </a>
-            {/* <a href="#" className="text-white">
+          <div>
+            <div className="flex items-center gap-x-8 mt-5">
+              <a href="#" className="text-white text-lg" onClick={changeLanguage}>
+                中 / EN
+              </a>
+              <a href="#content" className="text-white text-lg">
+                {t('header.content')}
+              </a>
+              <a href="#venue" className="text-white text-lg">
+                {t('header.venue')}
+              </a>
+              <a href="#speaker" className="text-white text-lg">
+                {t('header.speakers')}
+              </a>
+              {/* <a href="#" className="text-white">
               {t('header.nft')}
             </a> */}
-            {/* <a href="#" className="text-white">
+              {/* <a href="#" className="text-white">
               {t('header.sbt')}
             </a> */}
-            <a href="#about" className="text-white">
-              {t('header.about')}
-            </a>
-          </div>
-          <div className="social mt-3 flex ">
-            <img
-              src="/telegram.png"
-              alt="dali_web3"
-              onClick={() => openSocial('https://t.me/daliweb3')}
-            />
-            <img
-              src="/twitter.png"
-              alt="@dali_web3"
-              className="ml-8"
-              onClick={() => openSocial('https://twitter.com/dali_web3')}
-            />
-             <img
-              src="/github.png"
-              alt="@dali_web3"
-              className="ml-8"
-              onClick={() => openSocial('https://github.com/web3dali')}
-            />
-            <img
-              src="/jike.png"
-              alt="瓦猫之夏_大理web3街会"
-              className="ml-8"
-              onClick={() => openSocial('https://web.okjike.com/u/2B4FA9DE-D1D7-46BF-B2C0-DB16C73DDC55')}
-            />
+              <a href="#about" className="text-white text-lg">
+                {t('header.about')}
+              </a>
+            </div>
+            <div className="social mt-3 flex mt-5" style={{justifyContent:'center' }}>
+              <img
+                src="/telegram.png"
+                alt="dali_web3"
+                onClick={() => openSocial('https://t.me/daliweb3')}
+              />
+              <img
+                src="/twitter.png"
+                alt="@dali_web3"
+                className="ml-8"
+                onClick={() => openSocial('https://twitter.com/dali_web3')}
+              />
+              <img
+                src="/github.png"
+                alt="@dali_web3"
+                className="ml-8"
+                onClick={() => openSocial('https://github.com/web3dali')}
+              />
+              <img
+                src="/jike.png"
+                alt="瓦猫之夏_大理web3街会"
+                className="ml-8"
+                onClick={() =>
+                  openSocial(
+                    'https://web.okjike.com/u/2B4FA9DE-D1D7-46BF-B2C0-DB16C73DDC55'
+                  )
+                }
+              />
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="box-container flex-between pt-12 pb-8 border-b border-white text-white mt-[2rem]">
+          <div className="flex gap-4">
+            <img src={wamo} alt="summer of wamo" />
+            <div className="flex flex-col gap-2">
+              <div>Summer of WAMO</div>
+              <div className="text-sm">A Web3 Fest in Dali</div>
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-8">
+              <a href="#" className="text-white" onClick={changeLanguage}>
+                中 / EN
+              </a>
+              <a href="#content" className="text-white">
+                {t('header.content')}
+              </a>
+              <a href="#venue" className="text-white">
+                {t('header.venue')}
+              </a>
+              <a href="#speaker" className="text-white">
+                {t('header.speakers')}
+              </a>
+              {/* <a href="#" className="text-white">
+              {t('header.nft')}
+            </a> */}
+              {/* <a href="#" className="text-white">
+              {t('header.sbt')}
+            </a> */}
+              <a href="#about" className="text-white">
+                {t('header.about')}
+              </a>
+            </div>
+            <div className="social mt-3 flex ">
+              <img
+                src="/telegram.png"
+                alt="dali_web3"
+                onClick={() => openSocial('https://t.me/daliweb3')}
+              />
+              <img
+                src="/twitter.png"
+                alt="@dali_web3"
+                className="ml-8"
+                onClick={() => openSocial('https://twitter.com/dali_web3')}
+              />
+              <img
+                src="/github.png"
+                alt="@dali_web3"
+                className="ml-8"
+                onClick={() => openSocial('https://github.com/web3dali')}
+              />
+              <img
+                src="/jike.png"
+                alt="瓦猫之夏_大理web3街会"
+                className="ml-8"
+                onClick={() =>
+                  openSocial(
+                    'https://web.okjike.com/u/2B4FA9DE-D1D7-46BF-B2C0-DB16C73DDC55'
+                  )
+                }
+              />
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="flex-center text-white pt-8 pb-15">
         @ 2022 All Rights Reserved.
       </div>
