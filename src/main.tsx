835dfd4c1c0291a4a5d91a5e261ff37449008781
sanyuan <ziyuant@gmail.com>
2022-08-18 23:17:13 +0800

    feat(nft): 合约切换到主网

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,11 @@ import { getDefaultWallets, RainbowKitProvider, lightTheme, Theme } from '@rainb
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { infuraProvider } from 'wagmi/providers/infura'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-  [chain.rinkeby], // you can add more chains here like chain.mainnet, chain.optimism etc.
-  // [chain.mainnet],
+  // [chain.rinkeby], // you can add more chains here like chain.mainnet, chain.optimism etc.
+  [chain.mainnet],
   [
     alchemyProvider({ apiKey: '1YkQCuJSPjl5LU4mV_9dS_6P-sIzjDHR', priority: 0 }),
     infuraProvider({ apiKey: '15846464788c4374b845160a4c9457bd', priority: 1 }),
